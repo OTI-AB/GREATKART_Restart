1a8175703af41a19bfbf2ccf9dcaa1f26f6e3d30
Phillip <phillip.pearson.1018@gmail.com>
2025-09-07 11:18:12 -0600

    Restarting from Lesson 55 - Add Variation in cart item

---
+++ carts/views.py
@@ -0,0 +1,91 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.core.exceptions import ObjectDoesNotExist
+
+from carts.models import Cart, CartItem
+from store.models import Product, Variation
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        cart_total = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.item_price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (total * 7 / 100)
+        cart_total = (total + tax)
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'cart_total': cart_total,
+        'tax': tax,
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+    }
+
+
+    return render(request, 'store/cart.html', context)
+
+def increment_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    product_variation = []
+    if request.method == 'POST':
+       for item in request.POST:
+           key = item
+           value = request.POST[key]
+
+           try:
+               variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+               product_variation.append(variation)
+           except:
+               pass
+
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request),
+        )
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product = product,
+            quantity = 1,
+            cart = cart,
+        )
+        cart_item.save()
+    return redirect('cart')
+
+def decrement_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def delete_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
---
+++ store/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Product, Variation
+
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('item_name',)}
+    list_display = ('item_name', 'item_price', 'item_stock', 'item_category', 'updated_date', 'is_available',)
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value','is_active','created_date',)
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
